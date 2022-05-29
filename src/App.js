899f52124939dd7af75cf78b14676b5b181b4faf
Dmitry Pratsun <dpratsun@spscommerce.com>
2022-05-29 16:18:13 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>{likes}</h1>
+        <h1>{value}</h1>
+        <input type="text" value={value} onChange={event => setValue(event.target.value)} />
+        <button onClick={() => setLikes(likes + 1)} >Increment</button>
+        <button onClick={() => setLikes(likes - 1)} >Decrement</button>
     </div>
   );
 }
